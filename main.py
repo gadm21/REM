2bc3e1fed7950afa2b4677fee6035869daf7bf2e
gadm21 <g.gad@nu.edu.eg>
2024-01-25 20:48:43 -0500

    m

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from O365 import Message
+from O365 import Account, MSGraphProtocol  # same as from O365.connection import MSGraphProtocol
 import configparser
 from utils import * 
 
@@ -10,9 +10,21 @@ username_nu = config.get('credentials', 'username_nu')
 username_uwo = config.get('credentials', 'username_uwo')
 password_nu = config.get('credentials', 'password_nu')
 
-o365_auth = (username_nu, password_nu) 
-m = Message(auth=o365_auth)
-m.setRecipients(username_uwo)
-m.setSubject('I made an email script.')
-m.setBody('Talk to the computer, cause the human does not want to hear it any more.')
-m.sendMessage()
+credentials = (username_nu, password_nu)
+protocol = MSGraphProtocol(api_version='beta')  # MSGraphProtocol defaults to v1.0 api version
+account = Account(credentials, protocol=protocol)
+
+mailbox = account.mailbox()
+
+inbox = mailbox.inbox_folder()
+
+messages = inbox.get_messages()
+print("You have {} messages in your inbox".format(len(messages)))
+print(messages[0].subject)
+
+# o365_auth = (username_nu, password_nu) 
+# m = Message(auth=o365_auth)
+# m.setRecipients(username_uwo)
+# m.setSubject('I made an email script.')
+# m.setBody('Talk to the computer, cause the human does not want to hear it any more.')
+# m.sendMessage()
